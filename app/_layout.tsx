525e4daeb5b6304aefeb1f7924d4b825cc36637c
nimsimethusala <nimsimethusala42@gmail.com>
2025-09-17 00:01:36 +0530

    apply tailwind css

---
+++ app/_layout.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Slot } from 'expo-router'
+import '../global.css'
+
+const RootLayout = () => {
+  return (
+    <Slot />
+  )
+}
+
+export default RootLayout
---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Index = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-green-200">
+      <Text className='text-red-500'>Welcome to the GreenGarden</Text>
+    </View>
+  )
+}
+
+export default Index
