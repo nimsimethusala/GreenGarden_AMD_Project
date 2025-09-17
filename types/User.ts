39555779ea19115128511c0158d1440c613bad08
nimsimethusala <nimsimethusala42@gmail.com>
2025-09-17 21:24:10 +0530

    update user model

--- types/User.ts
+++ types/User.ts
@@ -1,11 +1,11 @@
 export type Role = "user" | "admin";
 
-export interface User {
+export interface UserProfile {
   id?: string;  
   username: string;
   email: string;
   phone?: string;
-  photoURL?: string;
+  photoURL?: string | null;
   role: Role;
   isDisabled?: boolean;
 }
