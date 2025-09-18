ac84c53834356d59fd956bc786e001ae620552b2
nimsimethusala <nimsimethusala42@gmail.com>
2025-09-18 16:37:25 +0530

    update implementation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,11 +3,8 @@ import { Slot } from 'expo-router'
 import '../global.css'
 import { ThemeProvider } from '@/context/ThemeContext'
 import { AuthProvider } from '@/context/AuthContext'
-// import { useRoleRedirect } from '@/hooks/useRoleRedirect'
 
 const RootLayout = () => {
-  // useRoleRedirect();
-
   return (
     <ThemeProvider>
       <AuthProvider>
