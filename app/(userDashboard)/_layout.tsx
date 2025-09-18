76db1589d555b58eab0809dba0221943fa4d5c58
nimsimethusala <nimsimethusala42@gmail.com>
2025-09-18 21:04:42 +0530

    add UserDashboardRoutes for tab navigation handling

---
+++ app/(userDashboard)/_layout.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Tabs } from 'expo-router'
+import { MaterialIcons } from "@expo/vector-icons"
+
+const tabs = [
+    {label: 'Home', name: 'home', icon: "home-filled"},
+    {label: 'Profile', name: 'userProfile', icon: "person" }
+] as const
+
+const UserDashboardRoutes = () => {
+  return (
+    
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#03fcb5",
+        tabBarInactiveTintColor: "#5f736d",
+        headerShown: false,
+        tabBarStyle: {
+          backgroundColor: "#001a12"
+        }
+      }}
+    >
+      {/* (obj.name) ===  ({name}) */}
+      {tabs.map(({ name, icon, label }) => (
+        <Tabs.Screen
+          key={name}
+          name={name}
+          options={{
+            title: label,
+            tabBarIcon: ({ color, size }) => (
+              <MaterialIcons name={icon} color={color} size={size} />
+            )
+          }}
+        />
+      ))}
+    </Tabs>
+  )
+}
+
+export default UserDashboardRoutes
