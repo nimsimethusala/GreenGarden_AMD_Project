c21a5ff0c351af0a85c76e7db748f9936c71adf9
nimsimethusala <nimsimethusala42@gmail.com>
2025-09-18 12:53:17 +0530

    update  userService

--- services/userService.ts
+++ services/userService.ts
@@ -1,46 +1,7 @@
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { auth, db, storage } from "@/firebase";
+import { db, storage } from "@/firebase";
 import { collection, deleteDoc, doc, getDoc, getDocs, setDoc, updateDoc } from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
-import { UserProfile, Role } from "@/types/User";
-
-// ========== SIGN UP ==========
-export const signup = async (options: { 
-    email: string; 
-    password: string; 
-    username: string; 
-    role?: Role; 
-    avatarBlob?: Blob | null 
-  }) => {
-    const { email, password, username, role = "user", avatarBlob } = options;
-    const cred = await createUserWithEmailAndPassword(auth, email, password);
-    const uid = cred.user.uid;
-
-    let photoURL: string | null = null;
-    if (avatarBlob) {
-      const storageRef = ref(storage, `avatars/${uid}.jpg`);
-      await uploadBytes(storageRef, avatarBlob);
-      photoURL = await getDownloadURL(storageRef);
-    }
-
-    const profile: UserProfile = {
-      id: uid,
-      username,
-      email,
-      photoURL,
-      role,
-      isDisabled: false,
-    };
-
-    await setDoc(doc(db, "users", uid), profile);
-
-    return profile;
-  }
-
-  // ========== LOGIN ==========
-  export const login = async (email: string, password: string) => {
-    return signInWithEmailAndPassword(auth, email, password);
-  };
+import { UserProfile } from "@/types/User";
 
   // ========== GET A USER PROFILE ==========
   export const getUser = async (uid: string): Promise<UserProfile | null> => {
