f4ef908c46d3fa1ad587226e7c308369d5e79823
nimsimethusala <nimsimethusala42@gmail.com>
2025-09-19 12:54:58 +0530

    add category model

---
+++ types/Category.ts
@@ -0,0 +1,9 @@
+export interface CategoryDoc {
+    id?: string;
+    name: string;
+    slug?: string;
+    description?: string;
+    createdBy: string;  
+    createdAt?: Date;
+    updatedAt?: Date;
+}
