fa9adf55e891d7b68f6a8b6ee86ef14a7667889e
nimsimethusala <nimsimethusala42@gmail.com>
2025-09-19 13:02:55 +0530

    implement get all categories logic

--- services/categoryService.ts
+++ services/categoryService.ts
@@ -1,5 +1,5 @@
 import { db } from "@/firebase";
-import { collection, doc, setDoc } from "firebase/firestore";
+import { collection, doc, setDoc, query, orderBy, getDocs } from "firebase/firestore";
 import { CategoryDoc } from "@/types/Category";
 
 const categoryCollection = collection(db, "categories");
@@ -15,4 +15,11 @@ export const addCategory = async (category: CategoryDoc): Promise<void> => {
 
   const categoryRef = category.id ? doc(db, "categories", category.id) : doc(categoryCollection);
   await setDoc(categoryRef, newCategory);
+};
+
+// ========== GET ALL CATEGORIES ==========
+export const getAllCategories = async (): Promise<CategoryDoc[]> => {
+  const q = query(categoryCollection, orderBy("createdAt", "desc"));
+  const snap = await getDocs(q);
+  return snap.docs.map((doc) => ({ id: doc.id, ...(doc.data() as CategoryDoc) }));
 };
